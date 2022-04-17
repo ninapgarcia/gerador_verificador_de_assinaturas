997aa48421ca59d5cfbfdd3cc1e19225d543efd2
Rafael-F-Barbosa <rafaelfbarbosa7@gmail.com>
2022-04-17 19:56:38 -0300

    erros

--- teste_hashlib.py
+++ teste_hashlib.py
@@ -1,11 +1,21 @@
-from cryptography.hazmat.primitives import hashes
+# from cryptography.hazmat.primitives import hashes
 
 
-digest = hashes.Hash(hashes.SHAKE128(128))
+# digest = hashes.Hash(hashes.SHAKE128(128))
 
-m = b"lalaland"
-print(m)
-digest.update(m)
+# m = b"lalaland"
+# print(m)
+# digest.update(m)
 
 
-print(len(digest.finalize()))
+# print(len(digest.finalize()))
+import hashlib 
+
+m = hashlib.sha3_224()
+
+m.update(b"lalaland")
+
+print(len(m.digest()))
+print(m.digest_size)
+print(m.hexdigest())
+print(int(m.hexdigest(), 10))
